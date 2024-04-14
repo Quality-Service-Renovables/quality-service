d253557239e62ee6103c1329f452d78d9a993077
Adrian Olvera <adrian.olvera21@gmail.com>
2024-04-13 22:56:41 -0600

    Crud - Categorias de Inspección - Crud

--- app/Http/Controllers/Api/V1/Inspections/CategoryController.php
+++ app/Http/Controllers/Api/V1/Inspections/CategoryController.php
@@ -6,10 +6,12 @@ use App\Http\Controllers\Controller;
 use App\Http\Modules\Api\V1\Inspections\CategoryModule;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
 {
     protected CategoryModule $module;
+
     public function __construct()
     {
         $this->module = new CategoryModule();
@@ -17,10 +19,13 @@ class CategoryController extends Controller
 
     /**
      * Display a listing of the resource.
+     *
+     * @throws \Exception
      */
     public function index(): JsonResponse
     {
         $this->module->read();
+
         return response()->json($this->module->response, $this->module->statusCode);
     }
 
@@ -37,21 +42,57 @@ class CategoryController extends Controller
      */
     public function store(Request $request): JsonResponse
     {
+        $validation = Validator::make($request->all(), [
+            'inspection_category' => 'required|string|min:10|max:255|unique:inspection_categories,inspection_category,NULL,inspection_category_uuid,deleted_at,NULL',
+            'description' => 'required|string|min:10|max:255',
+        ]);
+
+        if ($validation->fails()) {
+            $this->module->response = [
+                'message' => 'Validation error',
+                'errors' => $validation->errors(),
+            ];
+            $this->module->statusCode = 422;
+
+            return response()->json($this->module->response, $this->module->statusCode);
+        }
+
+        $this->module->create($request);
+
         return response()->json($this->module->response, $this->module->statusCode);
     }
 
     /**
      * Display the specified resource.
+     *
+     * @throws \Exception
      */
-    public function show(string $id): JsonResponse
+    public function show(string $uuid): JsonResponse
     {
+        $request = ['uuid' => $uuid];
+        $validation = Validator::make($request, [
+            'uuid' => 'required|string|min:10|max:255|exists:inspection_categories,inspection_category_uuid',
+        ]);
+
+        if ($validation->fails()) {
+            $this->module->response = [
+                'message' => 'Validation error',
+                'errors' => $validation->errors(),
+            ];
+            $this->module->statusCode = 422;
+
+            return response()->json($this->module->response, $this->module->statusCode);
+        }
+
+        $this->module->show($uuid);
+
         return response()->json($this->module->response, $this->module->statusCode);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id): JsonResponse
+    public function edit(string $uuid): JsonResponse
     {
         return response()->json($this->module->response, $this->module->statusCode);
     }
@@ -59,16 +100,55 @@ class CategoryController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id): JsonResponse
+    public function update(Request $request, string $uuid): JsonResponse
     {
+        // Add elements to request
+        $request->request->add(['inspection_category_uuid' => $uuid]);
+        // Validate
+        $validation = Validator::make($request->all(), [
+            'inspection_category_uuid' => 'required|string|min:10|max:255|exists:inspection_categories,inspection_category_uuid',
+            'inspection_category' => 'required|string|min:10|max:255|unique:inspection_categories,inspection_category,' . $request->inspection_category_uuid . ',inspection_category_uuid',
+            'description' => 'required|string|min:10|max:255',
+        ]);
+
+        // Validation fails
+        if ($validation->fails()) {
+            $this->module->response = [
+                'message' => 'Validation error',
+                'errors' => $validation->errors(),
+            ];
+            $this->module->statusCode = 422;
+
+            return response()->json($this->module->response, $this->module->statusCode);
+        }
+        // Module Processs
+        $this->module->update($request);
+        // Module Response
         return response()->json($this->module->response, $this->module->statusCode);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id): JsonResponse
+    public function destroy(string $uuid): JsonResponse
     {
+        $request = ['inspection_category_uuid' => $uuid];
+        $validation = Validator::make($request, [
+            'inspection_category_uuid' => 'required|string|min:10|max:255|exists:inspection_categories,inspection_category_uuid',
+        ]);
+
+        if ($validation->fails()) {
+            $this->module->response = [
+                'message' => 'Validation error',
+                'errors' => $validation->errors(),
+            ];
+            $this->module->statusCode = 422;
+
+            return response()->json($this->module->response, $this->module->statusCode);
+        }
+
+        $this->module->delete($uuid);
+
         return response()->json($this->module->response, $this->module->statusCode);
     }
 }
--- app/Http/Modules/Api/V1/Inspections/CategoryModule.php
+++ app/Http/Modules/Api/V1/Inspections/CategoryModule.php
@@ -1,21 +1,99 @@
 <?php
 
+/** @noinspection PhpUndefinedMethodInspection */
+
 namespace App\Http\Modules\Api\V1\Inspections;
 
 use App\Http\Modules\MainModule;
 use App\Models\InspectionCategory;
 use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class CategoryModule extends MainModule
 {
-    public function create()
+    public string $nameModule = 'inspection_category';
+
+    /**
+     * Create a new category.
+     *
+     * @param  \Illuminate\Http\Request  $request  The request object.
+     * @return array The response containing the created category.
+     */
+    public function create(Request $request): array
     {
-        //TODO Create Resource
+        try {
+            // Control Transaction
+            DB::beginTransaction();
+            // Complement Request
+            $data = $request->all();
+            $data['inspection_category_uuid'] = Str::uuid()->toString();
+            $data['inspection_category_code'] = $this->createSlug($request->get('inspection_category'));
+            // Create Register
+            InspectionCategory::create($data);
+            // Recover Register
+            $category = InspectionCategory::where('inspection_category_uuid', $data['inspection_category_uuid'])->first();
+            // Set Response
+            $this->response['statusCode'] = 201;
+            $this->response['message'] = 'Category created';
+            $this->response['data'] = ['category' => $category];
+            // Set Log
+            $this->logModule->create(
+                $this->nameModule,
+                $request->all(),
+                $this->response,
+                'Create new category inspection',
+                $request->user()->id,
+            );
+            // Commit Transaction
+            DB::commit();
+        } catch (Exception $exception) {
+            DB::rollBack();
+            $this->response['status'] = 'error';
+            $this->response['statusCode'] = 500;
+            $this->response['message'] = 'Unable to get list of categories'.$exception->getMessage();
+        }
+
+        // Response
+        return $this->response;
     }
 
-    public function update()
+    public function update(Request $request): array
     {
-        //TODO Update Resource
+        try {
+            // Control Transaction
+            DB::beginTransaction();
+            // Complement Request
+            $data = $request->all();
+            $data['inspection_category_code'] = $this->createSlug($request->get('inspection_category'));
+            // Create Register
+            InspectionCategory::where('inspection_category_uuid', $request->inspection_category_uuid)
+                ->update($data);
+            // Recover Register
+            $category = InspectionCategory::where('inspection_category_uuid', $request->inspection_category_uuid)->first();
+            // Set Response
+            $this->response['message'] = 'Category updated';
+            $this->response['data'] = ['category' => $category];
+            // Set Log
+            $this->logModule->create(
+                $this->nameModule,
+                $request->all(),
+                $this->response,
+                'Update new category inspection',
+                $request->user()->id,
+            );
+            // Commit Transaction
+            DB::commit();
+        } catch (Exception $exception) {
+            DB::rollBack();
+            $this->response['status'] = 'error';
+            $this->response['statusCode'] = 500;
+            $this->response['message'] = 'Unable update category: '.$exception->getMessage();
+        }
+
+        // Response
+        return $this->response;
     }
 
     /**
@@ -29,16 +107,77 @@ class CategoryModule extends MainModule
     {
         try {
             $this->response['message'] = 'List of Categories';
-            $this->response['data'] = ['categories' => InspectionCategory::all()];
+            $this->response['data'] = ['categories' => InspectionCategory::whereNull('deleted_at')->get()];
         } catch (Exception $exception) {
             $this->response['status'] = 'error';
-            $this->response['message'] = 'Unable to get list of categories'.$exception->getMessage();
+            $this->response['message'] = 'Unable to get list of categories: '.$exception->getMessage();
         }
+
         return $this->response;
     }
 
-    public function delete()
+    public function delete(string $uuid): array
     {
-        //TODO Delete Resource
+        try {
+            // Control Transaction
+            DB::beginTransaction();
+            // Delete Register
+            InspectionCategory::where('inspection_category_uuid', $uuid)
+                ->update([
+                    'deleted_at' => now(),
+                ]);
+            // Set Response
+            $this->response['message'] = 'Category deleted';
+            // Commit Transaction
+            DB::commit();
+        } catch (Exception $exception) {
+            DB::rollBack();
+            $this->response['status'] = 'error';
+            $this->response['statusCode'] = 500;
+            $this->response['message'] = 'Unable delete category: '.$exception->getMessage();
+        }
+
+        // Response
+        return $this->response;
+    }
+
+    /**
+     * Retrieve a specific category.
+     *
+     * @param  string  $uuid  The UUID of the category to retrieve.
+     * @return array The response containing the category.
+     *
+     * @throws \Exception If there is an error retrieving the category.
+     */
+    public function show(string $uuid): array
+    {
+        try {
+            $this->response['message'] = 'Show Category';
+            $category = InspectionCategory::where([
+                'inspection_category_uuid' => $uuid,
+                'deleted_at' => null
+            ])->first();
+            $this->response['data'] = compact('category');
+        } catch (Exception $exception) {
+            $this->response['status'] = 'error';
+            $this->response['message'] = 'Unable to show category: '.$exception->getMessage();
+        }
+
+        return $this->response;
+    }
+
+    public function createSlug(string $string): string
+    {
+        // Primero, reemplazar los caracteres no latinos por "-"
+        $slug = preg_replace('~[^\pL\d]+~u', '_', $string);
+
+        // A continuación, transcribimos cualquier carácter latino a ASCII
+        $slug = iconv('utf-8', 'us-ascii//TRANSLIT', $slug);
+
+        // Entonces, eliminamos los caracteres indeseables
+        $slug = preg_replace('~[^-\w]+~', '', $slug);
+
+        // Finalmente, convertimos el slug a minúscula y lo recortamos
+        return strtolower(trim($slug, '-'));
     }
 }
