27f5957807ec59b8c032a2cb26a1239d48a47b9f
Luis Adrian Olvera Facio <aolvera@taag-genetics.com>
2024-05-25 17:23:17 -0600

    * Laravel Pint

--- app/Models/Inspections/Categories/Section.php
+++ app/Models/Inspections/Categories/Section.php
@@ -13,20 +13,21 @@ class Section extends Model
     use HasFactory, SoftDeletes;
 
     protected $table = 'ct_inspection_sections';
+
     protected $primaryKey = 'ct_inspection_section_id';
+
     protected $fillable = [
         'ct_inspection_section_uuid',
         'ct_inspection_section',
         'ct_inspection_section_code',
         'ct_inspection_id',
-        'ct_inspection_relation_id'
+        'ct_inspection_relation_id',
     ];
+
     protected $hidden = ['ct_inspection_id'];
 
     /**
      * Get the category that belongs to this model.
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
     public function category(): BelongsTo
     {
