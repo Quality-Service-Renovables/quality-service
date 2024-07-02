c73bd937037edb7f2a23d69e62666dbc1fdc54f7
Prospero Cardenas <dev.cardenas7@gmail.com>
2024-07-02 17:01:40 -0600

    Modificación en front para agregar el riesgo a un campo

--- resources/js/Pages/Projects/Partials/Section.vue
+++ resources/js/Pages/Projects/Partials/Section.vue
@@ -36,20 +36,40 @@
                                                         Campo {{ field.required ? '*Requerido' :
                 'Opcional'
                                                         }}
-                                                        <v-switch v-model="field.switch_comment" color="secondary"
-                                                            label="Comentario" hide-details></v-switch>
+                                                        <div class="d-flex gap-5">
+                                                            <v-switch v-model="field.switch_comment" color="secondary"
+                                                                label="Comentario" hide-details></v-switch>
+                                                            <v-switch v-model="field.switch_risk" color="secondary"
+                                                                label="Riesgo" hide-details></v-switch>
+                                                        </div>
                                                     </v-card-subtitle>
                                                     <v-card-text class="pt-0">
                                                         <QuillEditor
                                                             v-model:content="field.content.inspection_form_value"
                                                             theme="snow" toolbar="essential" heigth="100%"
                                                             contentType="html" />
-                                                        <p class="mt-3 text-grey" v-if="field.switch_comment">Comentarios:</p>
+                                                        <!-- Comments Field -->
+                                                        <p class="mt-3 text-grey" v-if="field.switch_comment">
+                                                            Comentarios:</p>
                                                         <QuillEditor
                                                             v-model:content="field.content.inspection_form_comments"
                                                             theme="snow" toolbar="essential" heigth="100%"
-                                                            contentType="html" v-if="field.switch_comment"/>
-                                                        <PrimaryButton @click="saveField(field)" class="mt-2" :disabled="field.loading">Guardar
+                                                            contentType="html" v-if="field.switch_comment" />
+                                                        <!-- Risk Selector -->
+                                                        <p class="mt-3 text-grey" v-if="field.switch_risk">
+                                                            Riesgo:</p>
+                                                        <v-select v-model="field.content.risk_uuid" :items="risks"
+                                                            item-title="risk" item-value="risk_uuid" variant="outlined"
+                                                            hide-details v-if="field.switch_risk" class="w-50" density="compact">
+                                                            <template v-slot:item="{ props, item }">
+                                                                <v-list-item v-bind="props" :title="item.raw.risk"
+                                                                    :style="{ 'background-color': item.raw.color }"
+                                                                    value="risk"></v-list-item>
+                                                            </template>
+                                                        </v-select>
+                                                        <!-- Save Button -->
+                                                        <PrimaryButton @click="saveField(field)" class="mt-2"
+                                                            :disabled="field.loading">Guardar
                                                         </PrimaryButton>
                                                     </v-card-text>
                                                 </v-card>
@@ -73,7 +93,8 @@
                                                             <div v-if="subSection.fields">
                                                                 <v-card
                                                                     v-for="(fieldSub, indexFieldSub) in subSection.fields"
-                                                                    :key="indexFieldSub" class="my-5" :loading="fieldSub.loading">
+                                                                    :key="indexFieldSub" class="my-5"
+                                                                    :loading="fieldSub.loading">
                                                                     {{ complementData(fieldSub) }}
                                                                     <v-card-title class="d-flex justify-between">
                                                                         {{
@@ -89,23 +110,50 @@
                                                                         Campo {{
                 fieldSub.required ?
                     '*Requerido' :
-                                                                        'Opcional' }}
-                                                                        <v-switch v-model="fieldSub.switch_comment"
-                                                                            color="secondary" label="Comentario"
-                                                                            hide-details></v-switch>
+                    'Opcional' }}
+                                                                        <div class="d-flex gap-5">
+                                                                            <v-switch v-model="fieldSub.switch_comment"
+                                                                                color="secondary" label="Comentario"
+                                                                                hide-details></v-switch>
+                                                                            <v-switch v-model="fieldSub.switch_risk"
+                                                                                color="secondary" label="Riesgo"
+                                                                                hide-details></v-switch>
+                                                                        </div>
                                                                     </v-card-subtitle>
                                                                     <v-card-text>
                                                                         <QuillEditor
                                                                             v-model:content="fieldSub.content.inspection_form_value"
                                                                             theme="snow" toolbar="essential"
                                                                             heigth="100%" contentType="html" />
-                                                                        <p class="mt-3 text-grey" v-if="fieldSub.switch_comment">Comentarios:</p>
+                                                                        <!-- Comments Field -->
+                                                                        <p class="mt-3 text-grey"
+                                                                            v-if="fieldSub.switch_comment">Comentarios:
+                                                                        </p>
                                                                         <QuillEditor
                                                                             v-model:content="fieldSub.content.inspection_form_comments"
                                                                             theme="snow" toolbar="essential"
-                                                                            heigth="100%" contentType="html" v-if="fieldSub.switch_comment"/>
+                                                                            heigth="100%" contentType="html"
+                                                                            v-if="fieldSub.switch_comment" />
+                                                                        <!-- Risk Selector -->
+                                                                        <p class="mt-3 text-grey"
+                                                                            v-if="fieldSub.switch_risk">
+                                                                            Riesgo:</p>
+                                                                        <v-select v-model="fieldSub.content.risk_uuid"
+                                                                            :items="risks" item-title="risk"
+                                                                            item-value="risk_uuid" variant="outlined"
+                                                                            hide-details v-if="fieldSub.switch_risk"
+                                                                            class="w-50" density="compact">
+                                                                            <template v-slot:item="{ props, item }">
+                                                                                <v-list-item v-bind="props"
+                                                                                    :title="item.raw.risk"
+                                                                                    :style="{ 'background-color': item.raw.color }"
+                                                                                    value="risk"></v-list-item>
+                                                                            </template>
+                                                                        </v-select>
+                                                                        <!-- Save Button -->
                                                                         <PrimaryButton @click="saveField(fieldSub)"
-                                                                            class="mt-2" :disabled="fieldSub.loading">Guardar</PrimaryButton>
+                                                                            class="mt-2" :disabled="fieldSub.loading">
+                                                                            Guardar</PrimaryButton>
                                                                     </v-card-text>
                                                                 </v-card>
                                                             </div>
@@ -156,7 +204,39 @@ export default {
         return {
             dialogFormLoading: false,
             sectionsForm: [],
-            expandedPanel: [0, 1, 2, 3, 4, 5, 6]
+            expandedPanel: [0, 1, 2, 3, 4, 5, 6],
+            risks: [
+                {
+                    risk_uuid: 1,
+                    risk: 'Normal',
+                    color: '#9c0',
+                    description: 'Componente o elemento en buen estado, apto para seguir funcionando de manera normal y eficiente. En ocasiones puede observarse un desgaste propio del funcionamiento normal de la máquina.'
+                },
+                {
+                    risk_uuid: 2,
+                    risk: 'Leve',
+                    color: '#fffd99',
+                    description: 'Componente o elemento en el que se han apreciado daños o defectos leves. No impide el funcionamiento normal del aerogenerador, pero puede evolucionar de forma negativa. Debe realizarse un seguimiento de la máquina de forma que se inspeccione nuevamente en un plazo no superior a 1 año.'
+                },
+                {
+                    risk_uuid: 3,
+                    risk: 'Moderado',
+                    color: '#fc9',
+                    description: 'Componente o elemento en el que ya se aprecian daños o defectos desarrollados. Estos defectos evolucionarán con seguridad a una categoría superior y pueden poner en peligro a medio plazo la integridad de la máquina. Sin embargo, esto no impide el funcionamiento eficaz de la máquina. Debe realizarse un seguimiento de la máquina de forma que se inspeccione nuevamente en un plazo no superior a 6 meses.'
+                },
+                {
+                    risk_uuid: 4,
+                    risk: 'Grave',
+                    color: '#ff9901',
+                    description: 'Componente o elemento en el que se aprecian con claridad daños o defectos plenamente desarrollados. La máquina puede seguir funcionando, pero ello ocasionará un agravamiento muy rápido de los defectos presentes. Debe realizarse una vigilancia estrecha de la máquina de forma que se inspeccione con una periodicidad mensual para comprobar su evolución y poder anticiparse al fallo del componente.'
+                },
+                {
+                    risk_uuid: 5,
+                    risk: 'Peligro',
+                    color: '#ff0001',
+                    description: 'Existen daños severos en algún componente de la máquina que impiden o afectan gravemente a su buen funcionamiento. De seguir en funcionamiento, con seguridad se producirá un fallo catastrófico que puede dañar uno o más componentes, ocasionar daños a las personas o al medioambiente. Debe ser reparado de forma inmediata.'
+                }
+            ]
         }
     },
     methods: {
@@ -183,7 +263,8 @@ export default {
                 form: [{
                     inspection_form_value: field.content.inspection_form_value,
                     inspection_form_comments: field.content.inspection_form_comments,
-                    ct_inspection_form_uuid: field.ct_inspection_form_uuid
+                    ct_inspection_form_uuid: field.ct_inspection_form_uuid,
+                    risk_uuid: field.content.risk_uuid
                 }]
             }
 
@@ -199,9 +280,9 @@ export default {
                     .then(response => {
                         field.loading = false;
                         toast.success('Campo guardado correctamente');
-                        if(response.data.data.length > 0){
+                        if (response.data.data.length > 0) {
                             field.content = response.data.data[0];
-                        }else{
+                        } else {
                             this.getForm();
                         }
                     })
@@ -215,13 +296,16 @@ export default {
             if (field.content == null) {
                 field.content = {
                     inspection_form_value: '',
-                    inspection_form_comments: ''
+                    inspection_form_comments: '',
+                    risk_uuid: null
                 }
                 field.switch_comment = false;
-            }else{
+                field.switch_risk = false;
+            } else {
                 field.switch_comment = field.content.inspection_form_comments !== "" && field.content.inspection_form_comments !== null && field.content.inspection_form_comments !== "<p><br></p>" ? true : false;
+                field.switch_risk = field.content.risk_uuid && field.content.risk_uuid !== null ? true : false;
             }
-            
+
         },
         isEmptyField(field) {
             if (field.content.inspection_form_value == null || field.content.inspection_form_value == '') {
@@ -244,11 +328,11 @@ export default {
     }
 }
 
-.ql-toolbar.ql-snow{
+.ql-toolbar.ql-snow {
     border-radius: 15px 15px 0px 0px !important;
 }
 
-.ql-container.ql-snow{
+.ql-container.ql-snow {
     border-radius: 0px 0px 15px 15px !important;
 }
 </style>
