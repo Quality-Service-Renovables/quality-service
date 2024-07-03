ec24e2231db4145d299151f5722a1720d90dc849
Prospero Cardenas <dev.cardenas7@gmail.com>
2024-07-03 16:42:01 -0600

    avance edicion de foto de evidencia

--- resources/js/Pages/Projects/Partials/Evidence.vue
+++ resources/js/Pages/Projects/Partials/Evidence.vue
@@ -42,38 +42,14 @@
 <script>
 import { defineComponent } from 'vue'
 import { VueDraggableNext } from 'vue-draggable-next'
-
 import EvidenceForm from './EvidenceForm.vue';
 import { Toaster, toast } from 'vue-sonner'
 import { getInspection } from '@/Functions/api';
-// Import Vue FilePond
-import vueFilePond from "vue-filepond";
-
-// Import FilePond styles
-import "filepond/dist/filepond.min.css";
-
-// Import FilePond plugins
-// Please note that you need to install these plugins separately
-
-// Import image preview plugin styles
-import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css";
-
-// Import image preview and file type validation plugins
-import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
-import FilePondPluginImagePreview from "filepond-plugin-image-preview";
-import FilePondPluginImageEdit from "filepond-plugin-image-edit";
 import axios from 'axios';
 
-// Create component
-const FilePond = vueFilePond(
-    FilePondPluginFileValidateType,
-    FilePondPluginImagePreview,
-    FilePondPluginImageEdit
-);
 export default defineComponent({
     components: {
         draggable: VueDraggableNext,
-        FilePond,
         Toaster,
         EvidenceForm
     },
