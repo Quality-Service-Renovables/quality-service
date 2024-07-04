997c29d70ec63ac5cc1a7ea529e0c58ef57ea93a
Prospero Cardenas <dev.cardenas7@gmail.com>
2024-07-04 17:56:43 -0600

    cambios finales front

--- public/build/assets/api-CJCFeN5S.js
+++
@@ -1,1 +0,0 @@
-import{H as o}from"./app-Ba0Ttt2N.js";const t=o.create({baseURL:window.location.origin+"/api"}),e=i=>t.get(`/inspections/${i}`);export{e as g};
--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -11,11 +11,11 @@ defineProps({
 });
 </script>
 <template>
-    <v-btn
-        class="bg-grey-darken-4"
-        :loading="loading"
+    <button
+        class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
+        :disabled="loading"
     >
         <v-icon v-if="prependIcon" class="mr-2">{{ prependIcon }}</v-icon>
         <slot />
-    </v-btn>
+    </button>
 </template>
