3fbb87baaebddc4a71aa4492a034eb79af2c3893
Prospero Cardenas <dev.cardenas7@gmail.com>
2024-07-18 13:16:21 -0600

    Landing page

---
+++ public/index/wp-content/plugins/gutenverse/lib/dependencies/form.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-compose', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => 'f8038a759e7adc23b2d907a0bf561852');
