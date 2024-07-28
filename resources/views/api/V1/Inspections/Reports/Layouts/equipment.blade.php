f157f5259354559de179b1d83b3a0530010d96af
Prospero Cardenas <dev.cardenas7@gmail.com>
2024-07-27 19:34:21 -0600

    Ajuste en el blade de reportes para que jale los datos del equipo desde el json en inspections

--- resources/views/api/V1/Inspections/Reports/Layouts/equipment.blade.php
+++ resources/views/api/V1/Inspections/Reports/Layouts/equipment.blade.php
@@ -1,34 +1,15 @@
 <h3>Equipo</h3>
 <table class="inspection-table">
-    <tr>
-        <td><strong>Nombre: </strong></td>
-        <td colspan="2">{{$inspection->equipment->equipment}}</td>
-    </tr>
-    <tr>
-        <td><strong>Descripción: </strong></td>
-        <td colspan="2">{{$inspection->equipment->description}}</td>
-    </tr>
-    <tr>
-        <td><strong>Ubicación: </strong></td>
-        <td colspan="2">{{$inspection->location}}</td>
-    </tr>
-    <tr>
-        <td><strong>Fabricante del equipo: </strong></td>
-        <td>{{$inspection->equipment->model->trademark->trademark}}</td>
-        <td><strong>Año: </strong>{{$inspection->equipment->manufacture_date}}</td>
-    </tr>
-    <tr>
-        <td rowspan="2"><strong>Datos del equipo: </strong></td>
-        <td><strong>Modelo: </strong>{{$inspection->equipment->model->trademark_model}}</td>
-        <td><strong>Serie: </strong>{{$inspection->equipment->serial_number}}</td>
-    </tr>
-    <tr>
-        <td><strong>Horas Operación: </strong>{{$inspection->equipment->work_hours}}</td>
-        <td><strong>Energía Producida: </strong>{{$inspection->equipment->energy_produced}}</td>
-    </tr>
+    @php $equipment = json_decode($inspection->equipment_fields_report); @endphp
+    @foreach($equipment as $key => $value)
+        <tr>
+            <td><strong>{{ucfirst($value->name)}}: </strong></td>
+            <td>{{$value->value}}</td>
+        </tr>
+    @endforeach
 </table>
-<h3>Digrama Esquemático</h3>
-<img src="{{$inspection->equipment->equipment_diagram}}" width="45%" height="25%" style="margin: 10px">
+{{--<h3>Digrama Esquemático</h3>
+<img src="{{$inspection->equipment->equipment_diagram}}" width="45%" height="25%" style="margin: 10px">-->
 {{--    $pathAddress = asset('/').$inspection->equipment->equipment_diagram;--}}
 {{--  RESUMEN DE INSPECCIÓN  --}}
 <h3>Resumen de Inspección</h3>
