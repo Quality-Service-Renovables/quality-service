bf7aac0561932c737511895747a7859934effe5a
Prospero Cardenas <dev.cardenas7@gmail.com>
2024-08-15 14:27:48 -0600

    cambios en informe

--- resources/views/api/V1/Inspections/Reports/Layouts/header.blade.php
+++ resources/views/api/V1/Inspections/Reports/Layouts/header.blade.php
@@ -1,6 +1,6 @@
 <header>
    <div class="text-left">
     <img src="{{$inspection->provider->logo}}" style="max-width:125px;" alt="logo">
-    <p style="position:relative; font-size:12px; top:-30px; font-family: 'Arimo', sans-serif;letter-spacing: 0.55em;">www.qsr.mx</p>
+    <p style="position:relative; font-size:12px; top:-30px; letter-spacing: 0.53em; margin-left:3px;">www.qsr.mx</p>
    </div>
 </header>
