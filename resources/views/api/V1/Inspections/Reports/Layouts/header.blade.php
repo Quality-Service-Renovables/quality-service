77ee8d3a415b7b5de871a918905e90d47521a633
Prospero Cardenas <dev.cardenas7@gmail.com>
2024-10-11 10:41:40 -0600

    correcion menor en reporte

--- resources/views/api/V1/Inspections/Reports/Layouts/header.blade.php
+++ resources/views/api/V1/Inspections/Reports/Layouts/header.blade.php
@@ -1,6 +1,6 @@
 <header>
    <div class="text-left">
     <img src="{{$inspection->provider->logo}}" style="max-width:125px;" alt="logo">
-    <p style="position:relative; font-size:12px; top:-10px; letter-spacing: 0.53em; margin-left:3px;">www.qsr.mx</p>
+    <p style="position:relative; font-size:12px; top:-5px; letter-spacing: 0.53em; margin-left:3px;">www.qsr.mx</p>
    </div>
 </header>
